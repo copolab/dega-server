1163804a8471a93e6f65be376d43a54fa23cd80e
AMALU SAJEEV <amalu.sajeev.me@gmail.com>
2023-11-14 20:41:50 +0530

    feat: :sparkles: Added logger with pino

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { UserResolver } from './resolvers/user/Users.resolver';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -14,7 +15,17 @@ import { UserResolver } from './resolvers/user/Users.resolver';
       plugins: [
         ApolloServerPluginLandingPageLocalDefault()
       ]
-    })
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+        },
+        autoLogging: false,
+        quietReqLogger: true
+      }
+    }),
+    
   ],
   providers: [
     UserResolver,

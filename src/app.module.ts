8c798505f926f23f9930b785f1ec38146ce38528
AMALU SAJEEV <amalu.sajeev.me@gmail.com>
2023-11-17 00:53:51 +0530

    refactor: :art: code formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { UserResolver } from './resolvers/user/Users.resolver';
 import { Logger, LoggerModule } from 'nestjs-pino';
-import { EnvModule } from './env/env.module';
+import { EnvModule } from './config/env/env.module';
 import { ConfigModule } from '@nestjs/config';
-import { envSchema } from './env/env.schema';
+import { envSchema } from './config/env/env.schema';
 import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
-import { MongooseConfigService } from './mongoose-config/mongoose-config.service';
+import { MongooseConfigService } from './config/mongoose-config/mongoose-config.service';
 import { Connection } from 'mongoose';
+import { EnvService } from './config/env/env.service';
+import { CONNECTION_NAME } from './config/mongoose-config/constants';
 
 @Module({
   imports: [
@@ -37,27 +39,30 @@ import { Connection } from 'mongoose';
     }),
     EnvModule,
     MongooseModule.forRootAsync({
+      imports: [EnvModule],
       useClass: MongooseConfigService,
-      connectionName: 'dega-server'
+      connectionName: CONNECTION_NAME,
+      inject: [EnvService]
     })
   ],
   providers: [
     UserResolver,
     MongooseConfigService,
+    EnvService,
   ],
 })
 export class AppModule implements OnModuleInit{
   constructor(
     private readonly _scream: Logger,
-    @InjectConnection('dega-server') private readonly _dbConn: Connection
+    @InjectConnection(CONNECTION_NAME) private readonly _dbConn: Connection
   ){}
   onModuleInit() {
     if (this._dbConn.readyState === 1) {
-      this._scream.log('database connection succesfull', 'MongoDB');
+      this._scream.log('Database connection succesfull', 'MongoDB');
     }
     process.on('SIGINT', () => {
       this._dbConn.close();
-      this._scream.warn('closing database connection and exiting', 'Exit Process');
+      this._scream.warn('closing database connection and exiting', 'Process');
       process.exit(0);
     })
   }
